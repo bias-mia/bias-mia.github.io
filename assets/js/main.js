df0d6d9925567f81a8d453201a4ca938568b2143
AKA Math <amithjkamath@users.noreply.github.com>
2022-10-15 23:45:01 +0200

    attempting to add a map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 ---
----
+    ---
 
-{% include js/conference.js %}
+    {% include js / conference.js %}
+
+(() => {
+    const map = window.conference.map;
+
+    if (typeof map !== 'undefined') {
+        let main_station = L.marker([46.9480, 7.4474], {
+            icon: L.divIcon({
+                className: '',
+                html: '<span class="fas fa-train"></span> Uni Bern Main Building',
+                iconSize: [120, 56]
+            })
+        }).addTo(map);
+    }
+})();
